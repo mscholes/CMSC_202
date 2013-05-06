126f9fc4ec34698a993e73116b7e511422fca966
Matt Scholes <aumasch@gmail.com>
2013-05-06 11:53:34 -0400

    lab2(p3)
    Removed Scanner argument processing in favor of using primitive type
    checking tools. Not satisfied with the state field method for escaping
    when an exception is expected but NOT thrown.

--- LabAssignments/src/lab2/CommandArgs.java
+++ LabAssignments/src/lab2/CommandArgs.java
@@ -10,8 +10,6 @@
  */
 package lab2;
 
-import java.util.Scanner;
-
 public class CommandArgs {
 
 	/**
@@ -25,44 +23,59 @@ public class CommandArgs {
 	public static void main(String[] args) {
 		
 
-// Can't seem to get this to work because I need args to be readable for Scanner later. But if
-// I establish it as a string in the main setup above, I can't convert it to readable below...
-// I prolly don't understand the 'Readable' function, but I need to move on so I'll just skip
-// checking if there are 0 arguments provided.
-//
-// EDIT: Changed approach: now concatenating the array into a string for Scanner rather than
-//		 making the array Readable. This means the following check for 0 args will work.
-		if (args.length == 0) {
-			System.out.println("Proper usage is: java lab2.CommandArgs firstname lastname age");
+/**
+ *  Can't seem to get this to work because I need args to be readable for
+ *  Scanner later. But if I establish it as a string in the main setup above,
+ *  I can't convert it to readable below... I prolly don't understand the
+ *  'Readable' function, but I need to move on so I'll just skip checking if
+ *  there are 0 arguments provided.
+ *  
+ *  EDIT: Changed approach: now concatenating the array into a string for
+ *  	  Scanner rather than making the array Readable. This means the
+ *  	  following check for 0 args will work.
+ */
+		if (args.length < 3) {
+			System.out.println("Proper usage is:\n"
+							  + "java lab2.CommandArgs firstname lastname age");
 			System.exit(0);
 		}
-		
-// Doesn't work...
-//		Readable rargs = args;		
-		
-		// Concatenates the array 'args' into a space separated string sargs
-		String sargs = "";
-		for (String arg : args) {
-			sargs = sargs + ' ' + arg;
-		}
-		
-        Scanner input = new Scanner(sargs);
 
-		String nameFirst = input.next();
-		String nameLast  = input.next();
-		
-		while (input.hasNextInt()
-        		|| input.hasNextDouble()
-        		|| input.hasNextFloat()) {
-			System.out.print("!ERROR!\n  Enter age in word form!");
+/**
+ * This seems contrived using a state field to determine if I should escape
+ * the program. Is there a better way to invert the try/catch scheme to
+ * catch when an exception is NOT thrown?
+ */
+		boolean intrptCtrl = false;
+		for (String arg : args) {
+			try {
+				if (intrptCtrl == true) {
+					System.out.print("!ERROR!\n  Enter age in word form");
+					System.exit(0);
+				}
+				Float.valueOf(arg);
+				intrptCtrl = true;
+			} catch(NumberFormatException e) {
+				intrptCtrl = false;
+			}
+		}
+		// Necessary if the last arg is a number.
+		if (intrptCtrl == true) {
+			System.out.print("!ERROR!\n  Enter age in word form");
 			System.exit(0);
 		}
-		String age = input.nextLine();
+		
+		String nameFirst = args[0];
+		String nameLast  = args[1];
+		String age		 = args[2];
+		
+		if (args.length>3) {
+			for (int i = 3; i <= (args.length-1); i++){
+				age = age + " " + args[i];
+			}
+		}
 		
 		System.out.println("You Entered:\n" + nameLast + ", " + nameFirst + ", " + age);
-		
-		input.close();
-		
+				
 	}
 
 }
