ad5af1a13955a6430aca3b4280fdb8eec3a25fca
Matt Scholes <aumasch@gmail.com>
2013-06-25 12:59:35 -0400

    lab5
    Initial/final commit.

---
+++ Projects/src/proj1/CashRegister.java
@@ -0,0 +1,26 @@
+/**
+ * TODO Class description
+ * Class Invariants:
+ * TODO invariants list
+ * 
+ * @author Matt Scholes <mscholes3@gmail.com>
+ * @version Jun 15, 2013
+ *
+ */
+package proj1;
+
+public class CashRegister {
+
+	/**
+	 * TODO Method description
+	 * Precondition: None.
+	 * Postcondition: None.
+	 * @param 
+	 * @return 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Projects/src/proj1/Project1.java
@@ -0,0 +1,18 @@
+/**
+ * TODO Class description
+ * Class Invariants:
+ * TODO invariants list
+ * 
+ * @author Matt Scholes <mscholes3@gmail.com>
+ * @version Jun 15, 2013
+ *
+ */
+package proj1;
+
+public interface Project1 {
+	
+	public void main(String[] args){
+		
+	}
+	
+}
